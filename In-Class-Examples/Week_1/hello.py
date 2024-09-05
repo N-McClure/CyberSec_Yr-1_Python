7b85a021f0ea5f18ec58bb29d2c38ff393c1c8a6
Nick McClure <64433966+N-McClure@users.noreply.github.com>
2024-09-05 13:55:43 -0400

    Add files via upload

---
+++ In-Class-Examples/Week_1/hello.py
@@ -0,0 +1,72 @@
+# Saying Hello World: 
+print("Hello World!")
+
+print(" ")
+
+# Modifications made to the File:
+
+# Name and Intro:
+print(" My Name is Nick and I just received my Diploma from Sheridan for Computer Programming and am Ecstatic to be here.")
+
+print(" ")
+
+# Programming Experience and Length of Time using them:
+print("Here are a list of of Programming technologies I have used and how long I have been using them.")
+print(" ")
+
+print("*******************************")
+print("Front-End:")
+print(" ")
+
+print("- HTML 5 --> 2 years")
+print("- JavaScript --> 2 years")
+print("- CSS --> 2 years")
+print("- Angular --> 1.5 years")
+print("- TypeScript --> 1.5 years")
+print("- Windows Forms --> 4 monthes")
+print("- WPF --> 4 monthes")
+print("- XAML --> 4 monthes")
+print("*******************************")
+
+print("Back-End:")
+print(" ")
+
+print("- Java --> 4 years")
+print("- SpringBoot Framework --> 4 monthes")
+print("- OOP --> 4 years")
+print("- Python --> 2 years")
+print("- C# --> 4 monthes")
+print("- .Net Core --> 4 monthes")
+print("- .Net Standard --> 4 monthes")
+print("- TypeScript --> 1.5 years")
+print("- PHP --> 1 year")
+print("- MySQL --> 1.5 years")
+print("- MongoDB --> 1 year")
+print("*******************************")
+
+print("Hard-Ware / Robotics:")
+print(" ")
+
+print("- C++ --> 4 years")
+print("- Arduino --> 4 years")
+print("- Rasberry-Pi --> 1 year")
+
+print(" ")
+
+# Favourite Highschool Subjects:
+print("My fav Highschool subjects included; Woodshop, Auto, Physics, Computer Science, and Computer Engineering.")
+
+print(" ")
+
+# Least Favourite Highschool Subjects:
+print("My least fav subject was Biology.")
+
+print(" ")
+
+# My Hobbies:
+print("My hobbies...when I am not coding...Include cooking, camping, and building robots/electronics.")
+
+print(" ")
+
+# Anything else of Interest about Me:
+print("That is basically it...Nothing more to tell...That's Me.")
